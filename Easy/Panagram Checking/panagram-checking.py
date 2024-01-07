1454f11f7a4757f247d2bdded3921d213a276a6e
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-01-07 22:37:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Panagram Checking/panagram-checking.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if a string is Pangram or not.
+    def checkPangram(self,s):
+        #code here
+        a="abcdefghijklmnopqrstuvwxyz"
+        s=s.lower()
+        f=[]
+        for i in s:
+            if i in a:
+                if i not in f:
+                    f.append(i)
+        if len(f)==len(a):
+            return True
+        else:
+            return False
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        if(obj.checkPangram(s)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
