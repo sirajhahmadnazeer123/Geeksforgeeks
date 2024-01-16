5a31cbe6f6e8440ab4ba0dc01366399342279554
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-01-16 20:53:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Square root of a number/square-root-of-a-number.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+import math
+#Complete this function
+class Solution:
+    def floorSqrt(self, x): 
+    #Your code here
+        d=math.floor(math.sqrt(x))
+        return d
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            x=int(input())
+            
+            print(Solution().floorSqrt(x))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
