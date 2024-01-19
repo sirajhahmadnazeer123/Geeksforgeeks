cee8863ecda5942e723d84f5a8d3fa67317bb7b6
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-01-19 21:36:52 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sort an array according to the other/sort-an-array-according-to-the-other.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+from collections import Counter
+class Solution:
+    # A1[] : the input array-1
+    # N : size of the array A1[]
+    # A2[] : the input array-2
+    # M : size of the array A2[]
+    
+    #Function to sort an array according to the other array.
+    def relativeSort (self,A1, N, A2, M):
+        # Your Code Here
+        c = Counter(A1)
+        s = set(A1)
+        ans = []
+        for i in A2:
+            if i in s:
+                ans+=[i]*c[i]
+                s.remove(i)
+                
+        for i in sorted(list(s)):
+            ans+=[i]*c[i]
+            
+        return ans
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int (input ())
+    while t > 0:
+        n, m = list(map(int, input().split()))
+        a1 = list(map(int, input().split()))
+        a2 = list(map(int, input().split()))
+        
+        ob=Solution()
+        a1 = ob.relativeSort (a1, n, a2, m)
+        print (*a1, end = " ")
+        
+        print ()
+        
+        t -= 1
+
+# } Driver Code Ends
