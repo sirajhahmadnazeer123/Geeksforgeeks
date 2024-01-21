88f83bc575ff19be6988e48a49b03df5fb9e7993
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-01-21 20:54:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Replace a word/replace-a-word.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+    def replaceAll (ob, S, oldW, newW):
+        # code here 
+        X=S.replace(oldW,newW)
+        return(X)
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        S = input()
+        oldW = input()
+        newW = input()
+        
+        ob = Solution()
+        print(ob.replaceAll(S, oldW, newW))
+# } Driver Code Ends
