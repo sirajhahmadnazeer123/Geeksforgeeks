7ae5d681671147d23eced52050d860c1b8d99dc7
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-01-29 22:57:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sort a stack/sort-a-stack.py
@@ -0,0 +1,27 @@
+class Solution:
+    # your task is to complete this function
+    # function sort the stack such that top element is max
+    # funciton should return nothing
+    # s is a stack
+    def Sorted(self, s):
+        # Code here
+        return s.sort()
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.Sorted(arr)
+        for e in range(len(arr)):
+            print(arr.pop(), end=" ")
+        print()
+
+
+# } Driver Code Ends
