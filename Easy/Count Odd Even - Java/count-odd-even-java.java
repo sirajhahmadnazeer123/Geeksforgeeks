e7b07c225e17b1a2a50f326a09fc5b36ce349c93
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-02-01 21:20:23 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Odd Even - Java/count-odd-even-java.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+
+    // Driver code
+    public static void main(String[] args) {
+
+        // Taking input using scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking count of testcases
+        int testcase = sc.nextInt();
+
+        while (testcase-- > 0) {
+
+            // taking size of arary
+            int sizeof_array = sc.nextInt();
+
+            // creating an array
+            int a[] = new int[sizeof_array];
+
+            // inserting elements in the array
+            for (int i = 0; i < sizeof_array; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // creating an object of class Geeks
+            Geeks obj = new Geeks();
+
+            // calling countOddEven() method
+            // of class Geeks
+            obj.countOddEven(a, sizeof_array);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Geeks {
+    static void countOddEven(int a[], int n) {
+
+        int countOdd = 0;
+        int countEven = 0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0){
+                countEven++;
+            }
+            else if(a[i]%2!=0){
+                countOdd++;
+            }
+        }
+        System.out.print(countOdd +" " +countEven);
+    }
+}
+
