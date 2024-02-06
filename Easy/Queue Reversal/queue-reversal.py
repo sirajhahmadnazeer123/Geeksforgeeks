b99873b8735edfb29d4ca9fda64e63ee92c42387
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-02-06 10:59:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Queue Reversal/queue-reversal.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def rev(self, q):
+        ans = Queue()
+        for i in (list(q.queue))[::-1]: ans.put(i)
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+from queue import Queue
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        a=list(map(int,input().split()))
+        q=Queue(maxsize=n)
+        for j in a:
+            q.put(j)
+            
+        ob = Solution()
+        q=ob.rev(q)
+        for i in range(0,n):
+            print(q.get(),end=" ")
+        print()
+# } Driver Code Ends
