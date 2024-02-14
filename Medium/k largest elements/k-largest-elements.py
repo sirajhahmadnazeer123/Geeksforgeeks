52048f3c46bd663c29c49b302b7c56489ff8aaf3
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2024-02-14 21:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/k largest elements/k-largest-elements.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+class Solution:
+
+	def kLargest(self,arr, n, k):
+		# code here
+		p=list(arr)
+		p.sort()
+		p=p[::-1]
+		return p[0:k]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.kLargest(arr, n, k)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
